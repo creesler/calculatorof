734d7c2d713a278e868693a8d8e2e3a1e618baf6
creesler <creesler@gmail.com>
2025-06-29 23:21:41 +0800

    Add rewrite rule for sitemap.xml

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/sitemap.xml',
+        destination: '/sitemap',
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig 
