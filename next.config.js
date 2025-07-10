24e5a826654820a66390e7f8f469b4bf41e70861
creesler <creesler@gmail.com>
2025-07-10 23:36:52 +0800

    Update next.config.js with improved redirect handling

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'calculatorof.com',
+          }
+        ],
+        permanent: true,
+        destination: 'https://www.calculatorof.com/:path*',
+      }
+    ]
+  },
   async headers() {
     return [
       {
@@ -9,28 +24,11 @@ const nextConfig = {
           {
             key: 'Strict-Transport-Security',
             value: 'max-age=31536000; includeSubDomains'
-          },
+          }
         ],
-      },
+      }
     ]
-  },
-  async rewrites() {
-    return {
-      beforeFiles: [
-        // Handle www subdomain
-        {
-          source: '/:path*',
-          has: [
-            {
-              type: 'host',
-              value: 'www.calculatorof.com',
-            },
-          ],
-          destination: '/:path*',
-        },
-      ],
-    }
-  },
+  }
 }
 
 module.exports = nextConfig 
