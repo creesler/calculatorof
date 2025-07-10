3b3666ccc40a36c030039404f87329164df41a97
creesler <creesler@gmail.com>
2025-07-10 23:21:41 +0800

    Update next.config.js with rewrite rules and HSTS headers

--- next.config.js
+++ next.config.js
@@ -1,6 +1,36 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=31536000; includeSubDomains'
+          },
+        ],
+      },
+    ]
+  },
+  async rewrites() {
+    return {
+      beforeFiles: [
+        // Handle www subdomain
+        {
+          source: '/:path*',
+          has: [
+            {
+              type: 'host',
+              value: 'www.calculatorof.com',
+            },
+          ],
+          destination: '/:path*',
+        },
+      ],
+    }
+  },
 }
 
 module.exports = nextConfig 
